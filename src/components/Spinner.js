603c93773dd8fe862a2c7b27907328dbd20ba3d3
Nidhi Kumari <nsinha7295@gmail.com>
2024-05-11 12:52:08 -0400

    first commit

---
+++ src/components/Spinner.js
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+import loading from './loading.gif'
+
+export class Spinner extends Component {
+  render() {
+    return (
+      <div className='text-center'>
+        <img className='my-3' src={loading} alt="loading" />
+      </div>
+    )
+  }
+}
+
+export default Spinner
